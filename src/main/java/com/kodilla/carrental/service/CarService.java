99cfe4464c3e57aa41248801bde4bdc118ac1d46
Typhoonic <81630895+Typhoonic@users.noreply.github.com>
2021-11-13 16:37:19 +0100

    Implement CarController methods and fix CarService

--- src/main/java/com/kodilla/carrental/service/CarService.java
+++ src/main/java/com/kodilla/carrental/service/CarService.java
@@ -2,7 +2,6 @@ package com.kodilla.carrental.service;
 
 import com.kodilla.carrental.domain.Car;
 import com.kodilla.carrental.domain.CarDto;
-import com.kodilla.carrental.exception.CreatingObjectWithIdException;
 import com.kodilla.carrental.exception.EntityNotFoundException;
 import com.kodilla.carrental.mapper.CarMapper;
 import com.kodilla.carrental.repository.CarRepository;
@@ -22,23 +21,18 @@ public class CarService {
         return mapper.toDtoList(repository.findAll());
     }
 
-
     public CarDto getById(final Long carId) throws EntityNotFoundException {
         return repository.findById(carId).map(mapper::toDto).orElseThrow(() -> new EntityNotFoundException(Car.class, carId));
     }
 
-
-    public CarDto create(final CarDto carDto) throws CreatingObjectWithIdException {
-        if(carDto.getId() != 0) throw new CreatingObjectWithIdException(Car.class, carDto.getId());
+    public CarDto create(final CarDto carDto) {
         return saveAndMapToDto(carDto);
     }
-
 
     public CarDto update(final CarDto carDto) throws EntityNotFoundException {
         repository.findById(carDto.getId()).orElseThrow(() -> new EntityNotFoundException(Car.class, carDto.getId()));
         return saveAndMapToDto(carDto);
     }
-
 
     public void block(final Long carId) throws EntityNotFoundException {
         Car car = repository.findById(carId).orElseThrow(() -> new EntityNotFoundException(Car.class, carId));
@@ -46,15 +40,19 @@ public class CarService {
         repository.save(car);
     }
 
+    public void unblock(final Long carId) throws EntityNotFoundException {
+        Car car = repository.findById(carId).orElseThrow(() -> new EntityNotFoundException(Car.class, carId));
+        car.setBlock(false);
+        repository.save(car);
+    }
 
     public void delete(final Long carId) throws EntityNotFoundException {
         repository.findById(carId).orElseThrow(() -> new EntityNotFoundException(Car.class, carId));
         repository.deleteById(carId);
     }
 
-
     public String getContact() {
-        return "";
+        return "Contact";
     }
 
     private CarDto saveAndMapToDto(final CarDto carDto) {
